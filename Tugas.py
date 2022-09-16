3295c984b19c6ef4d6e3d1126262486665520758
Mohamad Fawaid Aska <113545522+Azkaame@users.noreply.github.com>
2022-09-16 13:26:41 +0700

    Add files via upload

---
+++ Tugas.py
@@ -0,0 +1,10 @@
+greeat=input('').lower ().replace(" ","")
+x=greeat[0:5]
+if(greeat=='hello'):
+    print("$0")
+elif(x=='hello'):
+    print("$0")
+elif(greeat[0]=='h'):
+    print("$20")
+else :
+    print("$100")  
